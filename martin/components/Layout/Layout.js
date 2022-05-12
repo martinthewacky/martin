760f30a4ad70ea961f6ba0e640c2492beb2d0e72
Martin Matovu <martinthewacky@gmail.com>
2022-05-12 03:05:01 +0300

    Edit Layout component for markup and styles

--- martin/components/Layout/Layout.js
+++ martin/components/Layout/Layout.js
@@ -1,7 +1,15 @@
 import style from "./Layout.module.css";
 
 const Layout = ({ children }) => {
-  return <div className={style.container}>{children}</div>;
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <div className={style.headerBg}></div>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}></footer>
+    </div>
+  );
 };
 
 export default Layout;
--- martin/pages/index.js
+++ martin/pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import Image from "next/image";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Martin Matovu</title>
       </Head>
-    </div>
+    </Layout>
   );
 }
